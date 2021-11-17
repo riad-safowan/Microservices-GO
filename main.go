6afbbe02827a56dc146a25e049ec3bb539255f25
Riad Safowan <riad.safowan@northsouth.edu>
2021-11-17 15:35:54 +0600

    git instruction

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 // curl -v -d 'amar sonar bangla'  localhost:9090 --- request with data to the server
 
 //git 
+// git init --- initialize directory to git repository
 // rm -rf .git --- delete git
 // git status --- status with current branch 
 // git branch --list --- list of branch
@@ -41,4 +42,6 @@ func main() {
 // git checkout branchname --- switch branch
 // git checkout -b branchname --- create and switch
 // git push --set-upstream origin branchname --- push with newly created branch
+// git merge branchname --- merge 'branchname' branch to current branch
+// git branch -d branchname --- delete local branch, -D for force delete
 
